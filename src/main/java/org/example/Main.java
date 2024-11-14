82d0427f16c88d5409c80c4456eb841990d339bb
Elakkiya <delakkiya2@gmail.com>
2024-11-14 22:15:19 +0530

    expressions in java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,26 @@
 package org.example;
-import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Scanner
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("name= ");
-        String name=scanner.nextLine();
-        System.out.println("what is your age?");
-        int age=scanner.nextInt();
-        //important to notice
-        scanner.nextLine();
-        System.out.println("whats your fav dish");
-        String favfood= scanner.nextLine();
-        System.out.println("name="+name);
-        System.out.println("age:"+age);
-        System.out.println("my fav dish is:"+favfood);
-        }
-
+         int friends=10;
+         friends=friends-1;
+        System.out.println(friends);
+         friends=friends+1;
+        System.out.println(friends);
+         friends=friends*9;
+        System.out.println(friends);
+         friends=friends/2;
+        System.out.println(friends);
+         friends=friends%2;
+         System.out.println(friends);
+         //increment operator
+        friends++;
+        System.out.println(friends);
+        friends--;
+        System.out.println(friends);
+        //integer division
+        double me=100;
+        me=me/3;
+        System.out.println(me);
+          }
 }
