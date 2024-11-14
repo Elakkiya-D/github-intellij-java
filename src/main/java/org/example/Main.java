67095fe040e36cdb6fa8c9d0ab8fa20f69b2c174
Elakkiya <delakkiya2@gmail.com>
2024-11-14 21:48:05 +0530

    scannerdetail in java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,15 @@ public class Main {
         Scanner scanner=new Scanner(System.in);
         System.out.println("name= ");
         String name=scanner.nextLine();
+        System.out.println("what is your age?");
+        int age=scanner.nextInt();
+        //important to notice
+        scanner.nextLine();
+        System.out.println("whats your fav dish");
+        String favfood= scanner.nextLine();
         System.out.println("name="+name);
-
+        System.out.println("age:"+age);
+        System.out.println("my fav dish is:"+favfood);
         }
 
 }
