f8e1d54e962faa7fbe26f153dfc439a21f7f2ba1
Elakkiya <delakkiya2@gmail.com>
2024-11-14 19:22:45 +0530

    variables and datatypes in java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,38 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //variables
+        //primitive datatype
+        int x;//initialization
+        x=100;//declaration
+        System.out.println(x);
+        System.out.println("x="+x);
+        long  y=789798767980980989L;
+        System.out.println("y="+y);
+        int z =130;
+        System.out.println("z="+z);
+        float a= 768.56F;
+        System.out.println("a="+a);
+        double c=6786;
+        System.out.println("c="+c);
+        boolean d=true;
+        System.out.println("d="+d);
+        char h='h';
+        System.out.println("h="+h);
+     //reference datatype
+        String name="Elakkiya";
+        System.out.println("my name is "+name);
       System.out.println("sri rama jayam");
+      //swapping of variables
+        String j="milk";
+        String k="water";
+        String temp;
+        //swap
+        temp=j;
+        j=k;
+        k=temp;
+        System.out.println("j:"+j);
+        System.out.println("k:"+k);
         }
 
 }
