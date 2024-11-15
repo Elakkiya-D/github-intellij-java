a2aececd718d868652d0837aed3d02046ce1e07c
Elakkiya <delakkiya2@gmail.com>
2024-11-15 19:35:43 +0530

    mathclass java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //type conversion/casting
-       double x=6.666;
-       int y=(int)x+1;
-       System.out.println(y);
-      //uncompatiple
-        String number="6";
-        int num=Integer.parseInt(number)+10;
+        //math class
+        double x=3.14;
+        double y=7.78;
+        double z=Math.max(x,y);
+        //double z=Math.abs(x);
+        //double z=Math.sqrt(y);
+        //double z=Math.round(x);
+        System.out.println(z);
+       int result =Math.round(6.9F);
+       double results=Math.random()*100;
+        int  s= (int) Math.round(Math.random()*100);
+        System.out.println(result);
+        System.out.println(results);
+        System.out.println(s);
 
-        System.out.println(num);
-        //constants
-        final float PI=3.14f;
-        System.out.println(PI);
           }
 }
