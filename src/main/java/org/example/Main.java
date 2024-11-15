d26316b6634db7fb8977901f0d279996fa85ab4b
Elakkiya <delakkiya2@gmail.com>
2024-11-15 20:51:06 +0530

    logicalOperators java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-       //comparison operators
-        int y=1;
-      int x=1;
-      System.out.println(x==y);
-      System.out.println(x!=y);
-        System.out.println(x<y);
-        System.out.println(x<=y);
-        System.out.println(x>=y);
-        System.out.println(x<y);
-          }
+        //logical operators
+        //AND
+        int temp=12;
+        boolean tem=temp<=12 && temp>13;
+         System.out.println(tem);
+         //OR
+        boolean highincome=true;
+        boolean credits=true;
+        boolean criminalrecords=false;
+        boolean eligibility=(highincome||credits) && !criminalrecords;
+        System.out.println(eligibility );
+    }
 }
