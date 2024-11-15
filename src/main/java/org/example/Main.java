3f761fe062ee14d5046ba12be86849e9f91a3310
Elakkiya <delakkiya2@gmail.com>
2024-11-15 18:25:12 +0530

    constants in java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //escape sequences
-        //here i want my name in double quotes
-        String name="my name is \"elakkiya\"";
-        System.out.println(name);
-        //newline
-        String newline="my name is \nelakkiya";
-        System.out.println(newline);
-        //tabspace
-        String tabSpace="my name is\telakkiya";
-        System.out.println(tabSpace);
-        //here i need slash\ in my code
-        String message="c:\\windows..\\...";
-        System.out.println(message);
+        //type conversion/casting
+       double x=6.666;
+       int y=(int)x+1;
+       System.out.println(y);
+      //uncompatiple
+        String number="6";
+        int num=Integer.parseInt(number)+10;
+
+        System.out.println(num);
+        //constants
+        final float PI=3.14f;
+        System.out.println(PI);
           }
 }
