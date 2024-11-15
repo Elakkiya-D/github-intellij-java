acb13c9fba9fd2d45c04e0e2b7278e5d6374ec2f
Elakkiya <delakkiya2@gmail.com>
2024-11-15 20:24:11 +0530

    comparisonOperators java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,20 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //math class
-        double x=3.14;
-        double y=7.78;
-        double z=Math.max(x,y);
-        //double z=Math.abs(x);
-        //double z=Math.sqrt(y);
-        //double z=Math.round(x);
-        System.out.println(z);
-       int result =Math.round(6.9F);
-       double results=Math.random()*100;
-        int  s= (int) Math.round(Math.random()*100);
-        System.out.println(result);
-        System.out.println(results);
-        System.out.println(s);
-
+       //comparison operators
+        int y=1;
+      int x=1;
+      System.out.println(x==y);
+      System.out.println(x!=y);
+        System.out.println(x<y);
+        System.out.println(x<=y);
+        System.out.println(x>=y);
+        System.out.println(x<y);
           }
 }
