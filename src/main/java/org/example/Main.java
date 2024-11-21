ce08b2940ef47129498286ab1c8b67adfc2365a3
Elakkiya <delakkiya2@gmail.com>
2024-11-21 21:46:47 +0530

    oops (object oriented program) java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,31 @@
 package org.example;
 
 import java.util.Scanner;
+ class car {
+     String colour = "red";
+     double Price = 20000.00;
+     String model = "swift";
+     int year = 2024;
 
-public class Main {
-    public static void main(String[] args) {
+     void drive() {
+         System.out.println("you can drive a car");
+     }
 
-     /*   Scanner scan= new Scanner(System.in);
-        System.out.println("press Q or q to quit the game");
-        String response=scan.next();
-        if(response.equals("q") || response.equals("Q")){
-            System.out.println("you quit the game");
-        }
-        else {
-            System.out.println("your still playing");
-        }*/
-        Scanner scan= new Scanner(System.in);
-        System.out.println("press Q or q to quit the game");
-        String response=scan.next();
-        if(response.equals("q") && !response.equals("Q")){
-            System.out.println("you quit the game");
-        }
-        else {
-            System.out.println("your still playing");
-        }
+      void brake() {
+         System.out.println("you can stop");
+     }
+ }
+    public class Main {
+    public static void main(String[] args){
+     car mycar=new car();
+     System.out.println(mycar.model);
+     System.out.println(mycar.Price);
+     mycar.drive();
+     mycar.brake();
+
+    }
+
 
 
     }
-}
+
